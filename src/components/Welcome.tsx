c3ce386c35e14f41fd48ded3c213f93b3e060350
Jessica Harriman <jess@perxhealth.com>
2024-10-18 21:08:07 +1100

    enlargen welcome logo

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,29 +1,33 @@
 import * as React from "react";
 import { Box, Stack, Heading, IconButton } from "@chakra-ui/react";
 import { ArrowDownIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 
 export const Welcome = () => {
   return (
     <Box backgroundImage="/nature.png">
-      <Stack>
-        <Stack align="center" justify="center" h="100vh" color="white">
-          <Heading fontFamily="Jost" pr="100px">
-            Melbourne
-          </Heading>
-          <Heading
-            size="4xl"
-            m="-9"
-            color="brand.500"
-            textShadow="1px 1px 2px grey"
-          >
-            Art
-          </Heading>
-          <Heading fontFamily="Jost" pl="110px">
-            Natural
-          </Heading>
-        </Stack>
+      <Stack align="center" justify="center" h="100vh" color="white">
+        <Heading fontFamily="Jost" pr="120px" fontSize="6xl">
+          Melbourne
+        </Heading>
+
+        <Heading
+          m="-16"
+          zIndex="900"
+          fontSize="9xl"
+          color="brand.500"
+          textShadow="1px 1px 2px grey"
+        >
+          Art
+        </Heading>
+
+        <Heading fontFamily="Jost" pl="180px" fontSize="6xl">
+          Natural
+        </Heading>
 
         <IconButton
+          as={Link}
+          to="/#about"
           fontSize="2xl"
           variant="ghost"
           color="brand.400"
