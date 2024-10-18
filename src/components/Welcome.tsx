bd5790d2fc3d85ad9954fd34093cd5630d9683b1
Jessica Harriman <jess@perxhealth.com>
2024-10-18 20:09:10 +1100

    redesign welcome logo

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,37 @@
 import * as React from "react";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Stack, Heading, IconButton } from "@chakra-ui/react";
+import { ArrowDownIcon } from "@chakra-ui/icons";
 
 export const Welcome = () => {
   return (
     <Box backgroundImage="/nature.png">
-      <Flex align="center" justify="center" h="100vh">
-        <Heading color="white">Melbourne Art Natural</Heading>
-      </Flex>
+      <Stack>
+        <Stack align="center" justify="center" h="100vh" color="white">
+          <Heading fontFamily="Jost" pr="100px">
+            Melbourne
+          </Heading>
+          <Heading
+            size="4xl"
+            m="-9"
+            color="brand.500"
+            textShadow="1px 1px 2px grey"
+          >
+            Art
+          </Heading>
+          <Heading fontFamily="Jost" pl="110px">
+            Natural
+          </Heading>
+        </Stack>
+
+        <IconButton
+          fontSize="2xl"
+          variant="ghost"
+          color="brand.400"
+          aria-label="scroll down"
+          icon={<ArrowDownIcon />}
+          _hover={{ bg: "none", color: "brand.400" }}
+        />
+      </Stack>
     </Box>
   );
 };
