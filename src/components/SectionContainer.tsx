725b30cfb54d9dc8924c960ca6206a905e620114
Jessica Harriman <jess@perxhealth.com>
2024-10-19 13:02:55 +1100

    add id prop

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -3,13 +3,15 @@ import { Box } from "@chakra-ui/react";
 import { SectionHeading } from "./SectionHeading";
 
 interface SectionContainerProps extends React.PropsWithChildren {
+  id?: string;
   label: string;
   colorScheme: "light" | "dark";
 }
 
 export const SectionContainer = (props: SectionContainerProps) => {
+  const id = props.id || props.label.toLowerCase();
   return (
-    <Box id={props.label.toLowerCase()} py="5" px="10">
+    <Box id={id} py="5" px="10">
       <SectionHeading {...props} />
       {props.children}
     </Box>
