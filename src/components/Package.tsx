5b39294a9e0e0f9652f1f82b65808bdf0c3a7de8
Jessica Harriman <jess@perxhealth.com>
2024-10-20 11:53:16 +1100

    extract package

---
+++ src/components/Package.tsx
@@ -0,0 +1,120 @@
+import * as React from "react";
+import { CheckCircleIcon, InfoOutlineIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+
+import {
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useToken,
+  List,
+  ListIcon,
+  ListItem,
+  Button,
+  Img,
+  Tooltip,
+} from "@chakra-ui/react";
+
+import { PackageEnum } from "../pages/Home";
+import { RatesProps } from "./Rates";
+
+export interface PackageProps extends RatesProps {
+  title: string;
+  description: string;
+  included: string[];
+  rate: number;
+  index: number;
+}
+
+export const Package = (props: PackageProps) => {
+  const { index, title, description, included, rate, setContactForm } = props;
+
+  const [green900] = useToken("colors", ["green.900"]);
+  const navigate = useNavigate();
+
+  const bookPackage = () => {
+    setContactForm((formValues) => ({
+      ...formValues,
+      preferredPackage: title.toLowerCase() as PackageEnum,
+    }));
+
+    navigate("/#contact");
+  };
+
+  const isOdd = (number: number) => {
+    return (number & 1) === 1;
+  };
+
+  return (
+    <Flex gap="10" align="center">
+      {isOdd(index) && <Img src="/nature.png" boxSize="sm" objectFit="cover" />}
+
+      <Stack gap="5" align="center" justify="center" h="100%" w="100%">
+        <Stack py="3" gap="5">
+          <Stack gap="1">
+            <Heading
+              as="i"
+              size="lg"
+              fontFamily="Jost"
+              _firstLetter={{
+                mr: "-0.5",
+                fontSize: "6xl",
+                fontFamily: "Imperial Script",
+                fontStyle: "normal",
+              }}
+            >
+              {title}
+            </Heading>
+            <Text fontSize="lg">{description}</Text>
+          </Stack>
+
+          <List spacing={3}>
+            {included.map((item) => (
+              <ListItem as={Flex} gap="2" align="center">
+                <ListIcon as={CheckCircleIcon} color="green.900" m="0" />
+                {item}{" "}
+                {item === "Complete Retouching" && (
+                  <Tooltip
+                    label="ie. airbrushing, teeth whitening, tattoo removal, blemish removal & colour grading"
+                    fontSize="md"
+                  >
+                    <InfoOutlineIcon />
+                  </Tooltip>
+                )}
+                {item === "Light Retouching" && (
+                  <Tooltip
+                    label="ie. blemish removal & colour grading"
+                    fontSize="md"
+                  >
+                    <InfoOutlineIcon />
+                  </Tooltip>
+                )}
+              </ListItem>
+            ))}
+          </List>
+
+          <Text fontSize="2xl">${rate}</Text>
+        </Stack>
+
+        <Button
+          onClick={bookPackage}
+          border={`solid 1px ${green900}`}
+          color="green.900"
+          bg="brand.200"
+          w="fit-content"
+          _hover={{
+            bg: "green.900",
+            color: "brand.200",
+          }}
+        >
+          Book this package
+        </Button>
+      </Stack>
+
+      {isOdd(index + 1) && (
+        <Img src="/nature.png" boxSize="sm" objectFit="cover" />
+      )}
+    </Flex>
+  );
+};
