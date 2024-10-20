9649aac4bd9f02ad5ffefd86afd1927680c99de7
Jessica Harriman <jess@perxhealth.com>
2024-10-20 16:39:48 +1100

    redesign gallery view

--- src/pages/GalleryView.tsx
+++ src/pages/GalleryView.tsx
@@ -1,55 +1,56 @@
 import * as React from "react";
-import { Box, Flex, Heading, Img, Stack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Img, Stack, Text } from "@chakra-ui/react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import { DotButton, useDotButton } from "../components/CarouselDots";
 import { gallery } from "./Gallery";
 import { useParams } from "react-router-dom";
 
 export const GalleryView = () => {
   let { set: setLabel } = useParams();
 
-  const [emblaRef, emblaApi] = useEmblaCarousel(
-    { loop: true, containScroll: false },
-    [Autoplay()]
-  );
-
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, axis: "y" }, [
+    Autoplay(),
+  ]);
 
   const set = gallery.find((set) => set.label.toLowerCase() === setLabel);
 
   return (
-    <Stack alignContent="space-between" justifyContent="space-between" h="98vh">
-      <Heading>{set?.label}</Heading>
+    <Flex alignContent="space-between" justifyContent="space-between" h="98vh">
+      <Heading pt="28" pl="10">
+        {set?.label}
+      </Heading>
 
-      <Stack gap="5" className="embla" align="center">
+      <Text position="fixed" bottom="10" left="10" w="30vw">
+        This is a description about this photo set. Additional information is
+        able to be displayed here. This is a description about this photo set.
+        Additional information is able to be displayed here. This is a
+        description about this photo set. Additional information is able to be
+        displayed here. This is a description about this photo set. Additional
+        information is able to be displayed here. This is a description about
+        this photo set. Additional information is able to be displayed here.
+      </Text>
+
+      <Flex gap="5" className="embla" align="center" overflow="hidden">
         <Box ref={emblaRef} className="embla__viewport" overflow="hidden">
-          <Flex className="embla__container" align="flex-end">
-            {set?.images.map((image, index) => (
-              <Box
-                pr="5"
-                minW="0"
-                className="embla__slide"
-                flex={selectedIndex === index ? "0 0 55%" : "0 0 35%"}
-              >
+          <Stack className="embla__container" align="flex-end" w="30vw">
+            {set?.images.map((image) => (
+              <Box pr="5" minW="0" className="embla__slide">
                 <Img src={image} />
               </Box>
             ))}
-          </Flex>
+          </Stack>
         </Box>
 
-        <Flex className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              isSelected={index === selectedIndex}
-              onClick={() => onDotButtonClick(index)}
-              className="embla__dot"
-            />
-          ))}
-        </Flex>
-      </Stack>
-    </Stack>
+        <Box ref={emblaRef} className="embla__viewport" overflow="hidden">
+          <Stack className="embla__container" align="flex-end" w="30vw">
+            {set?.images.map((image) => (
+              <Box pr="5" minW="0" className="embla__slide">
+                <Img src={image} />
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+      </Flex>
+    </Flex>
   );
 };
